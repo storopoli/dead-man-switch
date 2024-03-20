b2bccf456b152ab3697f5ac6945bfeaccd9fbdef
Jose Storopoli <jose@storopoli.io>
2024-03-20 18:47:29 -0300

    doc: add crate level doc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+//! # Dead Man's Switch
+//!
+//! This is a simple implementation of a
+//! [Dead Man's Switch](https://en.wikipedia.org/wiki/Dead_man%27s_switch)
+//!
+//! Use at your own risk.
+//! Check the f*(as in friendly) code.
 fn main() {
     println!("Hello, world!");
 }
