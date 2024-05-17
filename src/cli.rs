fc90bf141f7c6ca990ac57c23d06f30959d38970
jaoleal <jgleal@protonmail.com>
2024-05-17 12:33:28 -0300

    ]I Kissed it
    Signed-off-by: jaoleal <jgleal@protonmail.com>

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,13 @@
 use clap::Parser;
 
-use crate::config::config_path;
+use crate::config::default_path;
 
 /// CLI Arguments.
 #[derive(Parser, Debug)]
 #[clap(version, about, long_about = None)]
 pub struct DmsArgs {
     /// Path to TOML [`Config`](crate::config::Config).
-    #[clap(short, long, default_value_t = config_path().expect("Failed to get config path").to_str().unwrap().to_string())]
+    #[clap(short, long, default_value_t = default_path().to_str().unwrap().to_string() )]
     pub config: String,
 }
 
