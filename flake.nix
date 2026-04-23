fadea83d652c35bcc7da90fdc72168043b3f1219
Jose Storopoli <jose@storopoli.com>
2026-04-23 09:40:34 -0400

    chore: bump version to 0.9.3

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
 
         msrv = pkgs.rust-bin.stable."1.88.0".default;
 
-        package_version = "0.9.2";
+        package_version = "0.9.3";
 
         buildInputs = with pkgs; [
           bashInteractive
